bcc9f3dddd315547e5909fa1242ee7436702cfb7
henry_pheng <henry.pheng@gmail.com>
2020-10-30 12:50:03 +0100

    SCSS

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/User/UserDetail.vue
+++ src/views/User/UserDetail.vue
@@ -21,19 +21,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
   h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
+    color: red;
   }
 </style>
